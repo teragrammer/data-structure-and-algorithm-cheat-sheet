2d331328e87b93c1f87f5800e516a78e3a372045
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-09-21 10:54:11 -0400

    Update Q2 for Java with shifting array

--- 1.Arrays/Q2/Q2.java
+++ 1.Arrays/Q2/Q2.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
 
 public class Q2 {
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        int[] array = {1, 2, 3, 4, 5};
         int k = 3;
 
-        System.out.println(rotateArrayMethod1(array, k));
+        System.out.println(rotateArrayMethod1(arrayList, k));
+        System.out.println(Arrays.toString(rotateArrayMethod2(array, k)));
     }
 
     static ArrayList<Integer> rotateArrayMethod1(ArrayList<Integer> array, int k) {
@@ -29,6 +31,32 @@ public class Q2 {
                 array.add(0, lastItem);
                 counter++;
             }
+        }
+
+        return array;
+    }
+
+    static int[] rotateArrayMethod2(int[] array, int k) {
+        if (array.length == k || array.length == 0) {
+            return array;
+        }
+        
+        k = k % array.length;
+        int tempArray[] = new int[k];
+
+        // store last k elements in temp
+        for (int i = 0; i < k; i++) {
+            tempArray[i] = array[array.length - k + i];
+        }
+
+        // shift rest of array right by k
+        for (int i = array.length - k - 1; i >= 0; i--) {
+            array[i + k] = array[i];
+        }
+
+        // copy temp element to front
+        for (int i = 0; i < k; i++) {
+            array[i] = tempArray[i];
         }
 
         return array;
