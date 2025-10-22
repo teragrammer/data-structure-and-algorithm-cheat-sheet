4c901c7208d78c4654f4dd6a9e9c6fe03a5e741f
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-10-22 00:34:26 -0400

    Add: Create bubble sort method

--- problems/arrays/q1/Q1.php
+++ problems/arrays/q1/Q1.php
@@ -14,9 +14,8 @@ function sortedArrayMethod1($array = []) {
     for ($i = 0; $i < count($array); $i++) {
         $newArray[$i] = $array[$i] ** 2;
     }
-
-    sort($newArray);
-    return $newArray;
+    
+    return bubbleSort($newArray);
 }
 
 function sortedArrayMethod2($array = []) {
@@ -41,5 +40,23 @@ function sortedArrayMethod2($array = []) {
     return $newArray;
 }
 
-print_r(sortedArrayMethod1([5, 4, 3, 2, 1, 0]));
-print_r(sortedArrayMethod2([5, 4, 3, 2, 1, 0]));
+function bubbleSort($array = []) {
+    $size = count($array);
+
+    for ($i = 0; $i < $size - 1; $i++) {
+        for ($j = 0; $j < $size - $i - 1; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $array;
+}
+
+$array = [5, 4, 3, 2, 1, 0];
+
+print_r(sortedArrayMethod1($array));
+print_r(sortedArrayMethod2($array));
