c5eefbb8e2b494febfd0923446d8efb73df9dcdb
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-11-22 22:36:09 -0500

    Create two-sum for Dart

---
+++ grind-master-dsa/1.two-sum/Q1.dart
@@ -0,0 +1,45 @@
+List<int> twoSumMethod1(List<int> nums, int target) {
+    List<int> indices = List.filled(2, -1);
+    int size = nums.length;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) {
+                indices[0] = i;
+                indices[1] = j;
+                return indices;
+            }
+        }
+    }
+
+    return indices;
+}
+
+List<int> twoSumMethod2(List<int> nums, int target) {
+    List<int> indices = List.filled(2, -1);
+    int size = nums.length;
+    var lookup = {};
+
+    for (int i = 0; i < size; i++) {
+        int result = target - nums[i];
+
+        if (lookup.containsKey(result)) {
+            indices[0] = lookup[result];
+            indices[1] = i;
+            return indices;
+        }
+
+        lookup[nums[i]] = i;
+    }
+
+    return indices;
+}
+
+void main() {
+    List<int> nums = [6, 3, 1, 5, 2];
+    var target = 7;
+
+    print(twoSumMethod1(nums, target));
+    print(twoSumMethod2(nums, target));
+}
