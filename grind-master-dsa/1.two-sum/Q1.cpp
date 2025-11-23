a3b7ae458bd0b5d3cce1a7f68447027a0ea27bbe
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-11-22 22:35:57 -0500

    Create two-sum for C++

--- grind-master-dsa/1.two-sum/Q1.cpp
+++ grind-master-dsa/1.two-sum/Q1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 /*
@@ -15,7 +16,7 @@ int* twoSumMethod1(int nums[], int target, int size) {
     indices[1] = -1;
 
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size - 1; j++) {
+        for (int j = i + 1; j < size; j++) {
             int sum = nums[i] + nums[j];
             if (sum == target) {
                 indices[0] = i;
@@ -33,21 +34,39 @@ int* twoSumMethod2(int nums[], int target, int size) {
     int* indices = new int[2];
     indices[0] = -1;
     indices[1] = -1;
+    
+    unordered_map<int, int> lookup;
 
-    // TODO
+    for (int i = 0; i < size; i++) {
+        int neededNumber = target - nums[i];
+
+        if (lookup.find(neededNumber) != lookup.end()) {
+            indices[0] = lookup[neededNumber];
+            indices[1] = i;
+            return indices;
+        }
+
+        lookup[nums[i]] = i;
+    }
 
     return indices;
 }
 
 int main() {
     int nums[] = {6, 3, 1, 5, 2};
-    int target = 3;
+    int target = 7;
     int size = sizeof(nums) / sizeof(nums[0]);
 
     int* indices1 = twoSumMethod1(nums, target, size);
-    if (indices1[0] > 0 && indices1[1] > 0) {
-        cout << "Sum: " << nums[indices1[0]] + nums[indices1[1]] << 
+    if (indices1[0] != -1 && indices1[1] != -1) {
+        cout << "Method 1.) Sum: " << nums[indices1[0]] + nums[indices1[1]] << 
         " Indices: " << indices1[0] << ", " << indices1[1] << endl;
+    }
+
+    int* indices2 = twoSumMethod2(nums, target, size);
+    if (indices2[0] != -1 && indices2[1] != -1) {
+        cout << "Method 2.) Sum: " << nums[indices2[0]] + nums[indices2[1]] << 
+        " Indices: " << indices2[0] << ", " << indices2[1] << endl;
     }
 
     return 0;
