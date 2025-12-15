38d9707c4ead3283940ab160e39e5c1858287998
teragrammer <208279979+teragrammer@users.noreply.github.com>
2025-12-15 10:32:37 -0500

    Create Two Sum for Dart

--- grind-master-dsa/1.two-sum/Q1.dart
+++ grind-master-dsa/1.two-sum/Q1.dart
@@ -1,3 +1,9 @@
+/*
+* Given an array of integers nums and an integer target, 
+* return indices of the two numbers such that 
+* they add up to target.
+*/
+
 List<int> twoSumMethod1(List<int> nums, int target) {
     List<int> indices = List.filled(2, -1);
     int size = nums.length;
